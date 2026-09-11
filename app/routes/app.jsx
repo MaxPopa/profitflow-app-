0e48695149591a48a160b41b62c66556075ec7ca
MaxPopa <popam40@gmail.com>
2026-09-11 15:39:28 +0300

    Create app.jsx

---
+++ app/routes/app.jsx
@@ -0,0 +1,25 @@
+import { Link, Outlet } from "@remix-run/react";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { NavMenu } from "@shopify/app-bridge-react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+
+export default function App() {
+  return (
+    <AppProvider>
+      <NavMenu>
+        <Link to="/app" rel="home">Dashboard</Link>
+        <Link to="/app/dashboard2">Advanced Analytics</Link>
+        <Link to="/app/simulator">Profit Simulator</Link>
+        <Link to="/app/inventory">Inventory</Link>
+      </NavMenu>
+      <Outlet />
+    </AppProvider>
+  );
+}
+
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
